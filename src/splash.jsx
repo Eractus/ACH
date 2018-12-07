31716b3109ecee39b0b940208c362122cbdeb8e1
Danny Peng <dannyw.peng@gmail.com>
2018-12-06 17:22:39 -0800

    restyled splash page to be responsive to smaller screens

--- src/splash.jsx
+++ src/splash.jsx
@@ -25,10 +25,10 @@ class Splash extends React.Component {
               <p>What Inspires Us</p>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
+              <Link className="splash-button" to="/about"><button>Learn More</button></Link>
             </div>
             <img src="./images/splash7.jpg" />
           </article>
-          <Link className="splash-button" to="/about"><button>Learn More</button></Link>
         </section>
         <section className="splash-services darken-overlay">
           <p>What We Specialize In</p>
