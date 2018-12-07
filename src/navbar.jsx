6f098082af72a86c1e22d26ed53b7f0e66b8e84e
Danny Peng <dannyw.peng@gmail.com>
2018-12-06 16:09:26 -0800

    redesigned navbar and made it responsive to smaller screens

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,20 +2,48 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class Navbar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      toggled: false
+    };
+
+    this.toggleLinks = this.toggleLinks.bind(this);
+    this.collapseLinks = this.collapseLinks.bind(this);
+  }
+
+  collapseLinks() {
+    if (this.state.toggled) {
+      document.getElementById("toggle-navbar-links").className = "navbar-links";
+      this.setState({ toggled: false });
+    }
+  }
+
+  toggleLinks() {
+    if (this.state.toggled) {
+      document.getElementById("toggle-navbar-links").className = "navbar-links";
+      this.setState({ toggled: false });
+    } else {
+      document.getElementById("toggle-navbar-links").className += " show-navbar-links";
+      this.setState({ toggled: true })
+    }
+  }
+
   render() {
     return (
       <nav className="navbar-container">
         <Link className="navbar-logo" to="/">
-          <img src="./images/ACH-full-logo.png" />
+          <img src="./images/ACH-decal.png" />
         </Link>
-        <div className="navbar-links">
-          <Link to="/">HOME</Link>
-          <Link to="/about">ABOUT</Link>
-          <Link to="/services">SERVICES</Link>
-          <Link to="/gallery">GALLERY</Link>
-          <Link to="/reviews">REVIEWS</Link>
+        <div id="toggle-navbar-links" className="navbar-links">
+          <Link onClick={this.collapseLinks} to="/">HOME</Link>
+          <Link onClick={this.collapseLinks} to="/about">ABOUT</Link>
+          <Link onClick={this.collapseLinks} to="/services">SERVICES</Link>
+          <Link onClick={this.collapseLinks} to="/gallery">GALLERY</Link>
+          <Link onClick={this.collapseLinks} to="/reviews">REVIEWS</Link>
         </div>
         <p className="navbar-contact-number">714-679-4908</p>
+        <img onClick={this.toggleLinks} className="navbar-menu-icon" src="./images/hamburger.png" />
       </nav>
     )
   }
