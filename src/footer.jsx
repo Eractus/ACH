971b87df745fbf0adc7f9e535fd519d52a26c4bc
Danny Peng <dannyw.peng@gmail.com>
2018-12-12 19:46:30 -0800

    social media icons in footer and fixed some css in navbar

--- src/footer.jsx
+++ src/footer.jsx
@@ -5,6 +5,14 @@ class Footer extends React.Component {
     return (
       <footer className="footer-container">
         <img src="./images/ACH-font-logo.png" />
+        <div className="footer-social-media">
+          <a href="https://www.instagram.com/alchemycollectivehairlab/">
+            <img src="./images/instagram.png" />
+          </a>
+          <a href="https://www.yelp.com/biz/alchemy-collective-hair-lab-fountain-valley?osq=alchemy+collective">
+            <img src="./images/yelp.png" />
+          </a>
+        </div>
         <p>18438 BROOKHURST STREET</p>
         <p>FOUNTAIN VALLEY, CA 92708</p>
         <p>714-706-2948</p>
