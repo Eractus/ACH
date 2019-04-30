1a985dfc6c99b63a205822326cab2ead04128573
Danny Peng <dannyw.peng@gmail.com>
2019-04-30 13:40:54 -0700

    fixed footer social media links to open in new tab

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,13 +6,13 @@ class Footer extends React.Component {
       <footer className="footer-container">
         <img src="./images/ACH-font-logo.png" />
         <div className="footer-social-media">
-          <a className="footer-social-media-link" href="https://www.facebook.com/Alchemy-Collective-Hairlab-2219094228114532/">
+          <a className="footer-social-media-link" href="https://www.facebook.com/Alchemy-Collective-Hairlab-2219094228114532/" target="_blank">
             <img src="./images/facebook.png" />
           </a>
-          <a className="footer-social-media-link" href="https://www.instagram.com/alchemycollectivehairlab/">
+          <a className="footer-social-media-link" href="https://www.instagram.com/alchemycollectivehairlab/" target="_blank">
             <img src="./images/instagram.png" />
           </a>
-          <a className="footer-social-media-link" href="https://www.yelp.com/biz/alchemy-collective-hair-lab-fountain-valley?osq=alchemy+collective">
+          <a className="footer-social-media-link" href="https://www.yelp.com/biz/alchemy-collective-hair-lab-fountain-valley?osq=alchemy+collective" target="_blank">
             <img src="./images/yelp.png" />
           </a>
         </div>
