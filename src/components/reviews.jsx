837e857c90d733642221aa1401feb94d794defa6
Danny Peng <dannyw.peng@gmail.com>
2019-06-14 16:28:18 -0700

    refactor reviews page/component

--- src/components/reviews.jsx
+++ src/components/reviews.jsx
@@ -15,32 +15,54 @@ class Reviews extends React.Component {
           <section className="reviews-section-wrapper">
             <h3>Here's Some Love We've Received on Social Media - We Appreciate It!</h3>
             <ul className="reviews-list">
-              <li className="reviews-list-item">
-                <h5>Mikey messaged me after my not so delightful experience with a very honest and and genuinely concerned message ... did hair magic on my hair! ... Definitely recommend going to Mikey if you're looking to get services done here!</h5>
-                <p>-Patty P.</p>
-              </li>
-              <li className="reviews-list-item">
-                <h5>This salon is right in the corner of the plaza! ... I booked an appointment with Christina. Lemme tell you, she really cares about her clients! ... I'm soooo pleased with the results!...</h5>
-                <p>-Kathy N.</p>
-              </li>
-              <li className="reviews-list-item">
-                <h5>... more than 5 stars that's what i'd give michelle! ... overall a great person and hair stylist! i recommend her to everyone i meet!!</h5>
-                <p>-Becca P.</p>
-              </li>
-              <li className="reviews-list-item">
-                <h5>My new go to hair salon. This place is roomy ... Mikey knows how give the perfect cut every time.</h5>
-                <p>-Randy M.</p>
-              </li>
-              <li className="reviews-list-item">
-                <h5>Ask for Ryan!  I took my two teenage boys ... impressed and we'll be back!</h5>
-                <p>-Mike Z.</p>
-              </li>
+              {this.props.reviews.map((review, index) => (
+                <Review key={index} review={review} />
+              ))}
             </ul>
           </section>
         </main>
       </div>
     )
   }
+}
+
+Reviews.defaultProps = {
+  reviews: [
+    {
+      firstName: "Patty",
+      lastInitial: "P",
+      body: `Mikey messaged me after my not so delightful experience with a very honest and and genuinely concerned message ... did hair magic on my hair! ... Definitely recommend going to Mikey if you're looking to get services done here!`
+    },
+    {
+      firstName: "Kathy",
+      lastInitial: "N",
+      body: `This salon is right in the corner of the plaza! ... I booked an appointment with Christina. Lemme tell you, she really cares about her clients! ... I'm soooo pleased with the results!...`
+    },
+    {
+      firstName: "Becca",
+      lastInitial: "P",
+      body: `... more than 5 stars that's what i'd give michelle! ... overall a great person and hair stylist! i recommend her to everyone i meet!!`
+    },
+    {
+      firstName: "Randy",
+      lastInitial: "M",
+      body: `My new go to hair salon. This place is roomy ... Mikey knows how give the perfect cut every time.`
+    },
+    {
+      firstName: "Mike",
+      lastInitial: "Z",
+      body: `Ask for Ryan!  I took my two teenage boys ... impressed and we'll be back!`
+    },
+  ]
+}
+
+const Review = (props) => {
+  return (
+    <li className="reviews-list-item">
+      <h5>{props.review.body}</h5>
+      <p>-{props.review.firstName} {props.review.lastInitial}.</p>
+    </li>
+  )
 }
 
 export default Reviews;
